bebf4c8296a4f1b053377613869a7ede26f1dc56
Steven <senorvalenz@gmail.com>
2024-05-29 11:46:49 -0400

    Successful addition of feature to AI2PDF: choose btw 'folder' and 'open docs' modes.  No UI yet.

--- SV_AI2PDF.jsx
+++ SV_AI2PDF.jsx
@@ -1,5 +1,9 @@
 /**********************************************************
- 
+
+WIP:
+PROVIDING THE ABILITY TO WORK WITH EITHER OPENED FILES *OR* A FOLDER FULL O FILES, SEE #LOA
+
+
 DESCRIPTION
  
 This sample gets files specified by the user from the 
@@ -32,53 +36,46 @@ Forked from Swasher
 
 **********************************************************/
 
-// replace pdfSaveOpts.pDFPreset  = '1.4'; with your distiller preset
-
 // uncomment to suppress Illustrator warning dialogs
 app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
 
+var destFolder, files, sourceDoc, targetFile, pdfSaveOpts;
 
+/**********************************************************
+SETTINGS!
+**********************************************************/
+var useOpenFiles = true;
+var embed = true;
 
-var destFolder, sourceFolder, files, fileType, sourceDoc, targetFile, pdfSaveOpts;
- 
-// Select the source folder.
-sourceFolder = Folder.selectDialog( 'Select the folder with Illustrator .ai files you want to convert to PDF');
+/**********************************************************
+MAIN LOOP
+**********************************************************/
+if (useOpenFiles == false) {
+    files = getFilesFromFolder();
 
- 
-// If a valid folder is selected
-if ( sourceFolder != null )
-{
-    files = new Array();
-    fileType = "*.ai"; //prompt( 'Select type of Illustrator files to you want to process. Eg: *.ai', ' ' );
- 
-    // Get all files matching the pattern
-    files = sourceFolder.getFiles( fileType );
- 
     if ( files.length > 0 )
     {
         // Get the destination to save the files
         destFolder = Folder.selectDialog( 'Select the folder where you want to save the converted PDF files.', '~' );
         
-        logPath = destFolder+'/log.txt';
-        var logFile = File(logPath);
-        logFile.open('a'); // Open in append mode
-        logFile.writeln('This log was created by SV_AI2PDF.jsx (Adobe Illustrator)');
-        // Write the current date and time
-        logFile.writeln('Timestamp: ' + getCurrentTimestamp());
+        logFile = createLog(destFolder+'/log.txt')
 
         for ( i = 0; i < files.length; i++ )
         {
             sourceDoc = app.open(files[i]); // returns the document object
- 
-            embed_links(logFile);  // Comment this function out if you wish NOT to embed links
+
+            if (embed == true) {
+                embed_links(logFile, sourceDoc);  // Comment this function out if you wish NOT to embed links
+            }
 
             // Call function getNewName to get the name and file to save the pdf
             targetFile = getNewName();
             
             // Call function getPDFOptions get the PDFSaveOptions for the files
             pdfSaveOpts = getPDFOptions();
- 
+
             // Save as pdf
+            logMessage(logObject, sourceDoc.name)   
             sourceDoc.saveAs(targetFile,pdfSaveOpts);
 
             sourceDoc.close();
@@ -92,11 +89,124 @@ if ( sourceFolder != null )
     }
 }
 
+else {
+    if (app.documents.length > 0 ) {
+
+        // Get the folder to save the files into
+        var destFolder = null;
+        destFolder = Folder.selectDialog( 'Select folder for PDF files.', '~' );
+        
+        if (destFolder != null) {
+            var pdfSaveOptions, i, sourceDoc, targetFile;	
+            
+            logObject = createLog(destFolder + '/log.txt')
+
+            
+            // Get the PDF options to be used
+            pdfSaveOptions = getPDFOptions();
+                                    
+            for ( i = 0; i < app.documents.length; i++ ) {
+                sourceDoc = app.documents[i]; // returns the document object
+                logMessage(logObject, sourceDoc.name)
+
+                if (embed == true) {
+                    embed_links(logObject, sourceDoc);  // Comment this function out if you wish NOT to embed links
+                }
+
+                // Get the file to save the document as pdf into
+                targetFile = this.getTargetFile(sourceDoc.name, '.pdf', destFolder);
+                
+                // Save as pdf
+                sourceDoc.saveAs( targetFile, pdfSaveOptions );
+                // close the doc so you don't have to close a bunch of PDFs manually
+                sourceDoc.close();
+            }
+            logObject.close();
+            alert( 'Files are saved as PDF in ' + destFolder );
+        }
+    }
+    else{
+        throw new Error('There are no documents open!');
+    }
+}
+
+
+/**********************************************************
+FUNCTIONS
+**********************************************************/
+
+/*
+This function is from the sample script that ships with Illustrator
+*/
+function getTargetFile(docName, ext, destFolder) {
+	var newName = "";
+
+	// if name has no dot (and hence no extension),
+	// just append the extension
+	if (docName.indexOf('.') < 0) {
+		newName = docName + ext;
+	} else {
+		var dot = docName.lastIndexOf('.');
+		newName += docName.substring(0, dot);
+		newName += ext;
+	}
+	
+	// Create the file object to save to
+	var myFile = new File( destFolder + '/' + newName );
+	
+	// Preflight access rights
+	if (myFile.open("w")) {
+		myFile.close();
+	}
+	else {
+		throw new Error('Access is denied');
+	}
+	return myFile;
+}
+
 /* 
 Function to log a message to the text file
     log = log object creaed with File()
     message = string, will be written to file
 */
+
+function getOpenFiles(){
+    var files
+    // #LOA, uh oh, realizing I'm not really sure what I'm doing here!
+
+
+}
+
+function getFilesFromFolder() {
+    // Select the source folder.
+    var sourceFolder;
+    var files;
+    var fileType;
+    sourceFolder = Folder.selectDialog( 'Select the folder with Illustrator .ai files you want to convert to PDF');
+    
+    // If a valid folder is selected
+    if ( sourceFolder != null )
+    {
+        files = new Array();
+        fileType = "*.ai"; //prompt( 'Select type of Illustrator files to you want to process. Eg: *.ai', ' ' );
+    
+        // Get all files matching the pattern
+        files = sourceFolder.getFiles( fileType );
+    }
+    return files
+}
+
+function createLog(logPath) {
+    var logFile = File(logPath);
+    logFile.open('a'); // Open in append mode
+    logFile.writeln('');
+    logFile.writeln('');
+    logFile.writeln('This log was created by SV_AI2PDF.jsx (Adobe Illustrator)');
+    // Write the current date and time
+    logFile.writeln(getCurrentTimestamp());
+    return logFile
+}
+
 function logMessage(logObject, message) { 
     logObject.writeln(message);
 }
@@ -109,21 +219,25 @@ function getCurrentTimestamp() {
     return timestamp;
 }
 
-
-function embed_links(logObject) {
+// Embed links and log to file
+function embed_links(logObject,sourceDoc) {
     // Get all linked items in the document
-    var linkedItems = sourceDoc.placedItems;
-
-    // Loop through each linked item and embed it
-    for (var i = 0; i < linkedItems.length; i++) {
-        var linkedItem = linkedItems[i];
-        try {
-            linkedItem.embed();
-            // If successful, log a message
-            logMessage(logObject, "Embedded: " + linkedItem.file.name);
-        } catch (e) {
-            // If embedding fails, log an error message
-            logMessage(logObject, "Error embedding: " + linkedItem.file.name);
+    var link_count = sourceDoc.placedItems.length;
+    if (link_count > 0) {
+        // logMessage(logObject, "Embedding " + link_count + " links...");
+        var linkedItems = sourceDoc.placedItems;
+    
+        // Loop through each linked item and embed it
+        for (var i = 0; i < linkedItems.length; i++) {
+            var linkedItem = linkedItems[i];
+            try {
+                linkedItem.embed();
+                // If successful, log a message
+                logMessage(logObject, "Embedded: " + linkedItem.file.name);
+            } catch (e) {
+                // If embedding fails, log an error message
+                logMessage(logObject, "Error embedding: " + linkedItem.file.name);
+            }
         }
     }
 }
